79bee9f94694951de028701f6d22075189ef5f52
Nathan Lewis <n.lewis.perc@gmail.com>
2020-11-25 19:35:12 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,9 +4,11 @@ require 'pry'
 
 def key_for_min_value(name_hash)
   min_value = name_hash.first[1]
+  min_key = name_hash.first[0]
   name_hash.collect do |key, index|
     if index - min_value < 0
       min_value = index
+      min_key = key
     end
    min_value
   end
